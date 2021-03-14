19f1a801142e099971cdaecbcd1b414bd920c585
Emil <xairov.emil@gmail.com>
2021-03-15 03:24:26 +0400

    Implemented Hashing
    Added changes to methods that now consider hashing.
    
    Hash & check if plaintext & hashed passwords match.
    
    Pretty much finished.

--- src/main/java/utils/DBUtils.java
+++ src/main/java/utils/DBUtils.java
@@ -1,5 +1,7 @@
 package utils;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.sql.*;
 
 public class DBUtils {
@@ -27,11 +29,26 @@ public class DBUtils {
         return connection;
     }
 
-    public Connection connExternal(){
+    public Connection connExternal() {
         loadDriver(dbDriver);
         return getConnection();
     }
 
+    // used to hash password
+    private String hashPassword(String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
+
+    // used to validate hashed pass with entered plaintext
+    private boolean checkPass(String plainPassword, String hashedPassword) {
+        if (BCrypt.checkpw(plainPassword, hashedPassword)) {
+            System.out.println("The password matches.");
+            return true;
+        } else {
+            System.out.println("The password does not match.");
+        }
+        return false;
+    }
 
 
     public String insert(User user) {
@@ -41,7 +58,8 @@ public class DBUtils {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, user.getEmail());
-            preparedStatement.setString(2, user.getPassword());
+            // hashing and passing simultaneously
+            preparedStatement.setString(2, hashPassword(user.getPassword()));
             preparedStatement.setInt(3, user.getAge());
             preparedStatement.setString(4, user.getCountry());
             preparedStatement.setString(5, user.getCity());
@@ -57,18 +75,31 @@ public class DBUtils {
         return null;
     }
 
-    public String update(User user) {
+    public boolean update(User user) {
         loadDriver(dbDriver);
         Connection connection = getConnection();
-        String removeSQL = "delete from users where email=? and password=?";
-        try{
-            System.out.println("Removing old record...");
-          PreparedStatement preparedStatement = connection.prepareStatement(removeSQL);
-          preparedStatement.setString(1, user.getEmail());
-          preparedStatement.setString(2, user.getPassword());
-            preparedStatement.executeUpdate();
-          preparedStatement.close();
-        }catch (SQLException e){
+        boolean status = false;
+        String removeSQL = "delete from users where email=?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(removeSQL);
+            preparedStatement.setString(1, user.getEmail());
+            // preparedStatement.setString(2, hashPassword(user.getPassword()));
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            status = resultSet.next();
+
+            while(status){
+                String hashedPass = resultSet.getString("password");
+
+                if(checkPass(user.getPassword(), hashedPass)){
+                    System.out.println("Found User");
+                    break;
+                } else {
+                    System.out.println("No matches...Failed to remove old record");
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
             System.out.println("Failed to remove old record");
             e.printStackTrace();
         }
@@ -78,7 +109,8 @@ public class DBUtils {
             System.out.println("Inserting updated record...");
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, user.getEmail());
-            preparedStatement.setString(2, user.getPassword());
+            // hashing and passing
+            preparedStatement.setString(2, hashPassword(user.getPassword()));
             preparedStatement.setInt(3, user.getAge());
             preparedStatement.setString(4, user.getCountry());
             preparedStatement.setString(5, user.getCity());
@@ -86,38 +118,48 @@ public class DBUtils {
             preparedStatement.setString(7, user.getName());
             preparedStatement.setString(8, user.getSurname());
             preparedStatement.executeUpdate();
+            System.out.println("Inserted!");
             preparedStatement.close();
             connection.close();
+            return true;
         } catch (SQLException e) {
             System.out.println("Failed to insert updated record");
             e.printStackTrace();
         }
 
-        return null;
+        return false;
     }
-
-
-
-
-
 
 
     public boolean validate(User user) {
         loadDriver(dbDriver);
         Connection connection = getConnection();
         boolean status = false;
-        String sql = "select * from users where email = ? and password = ?";
+        String sql = "select * from users where email = ?";
 
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, user.getEmail());
-            preparedStatement.setString(2, user.getPassword());
+           // preparedStatement.setString(2, hashPassword(user.getPassword()));
             ResultSet resultSet = preparedStatement.executeQuery();
 
             status = resultSet.next();
+
+            while(status){
+                String hashedPass = resultSet.getString("password");
+
+                if(checkPass(user.getPassword(), hashedPass)){
+                    System.out.println("Found User");
+                    return status;
+                } else {
+                    System.out.println("No matches");
+                    return false;
+                }
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
 
         return status;
     }
@@ -126,25 +168,34 @@ public class DBUtils {
         loadDriver(dbDriver);
         Connection connection = getConnection();
         boolean status = false;
-        String sql = "select * from users where email = ? and password = ?";
+        String sql = "select * from users where email = ?";
 
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, user.getEmail());
-            preparedStatement.setString(2, user.getPassword());
+            //preparedStatement.setString(2, user.getPassword());
             ResultSet resultSet = preparedStatement.executeQuery();
             status = resultSet.next();
 
-            if(status){
-                user.setName(resultSet.getString("name"));
-                user.setSurname(resultSet.getString("surname"));
-                user.setAge(resultSet.getInt("age"));
-                user.setCountry(resultSet.getString("country"));
-                user.setCity(resultSet.getString("city"));
-                user.setGender(resultSet.getString("gender"));
+            while(status){
+                String hashedPass = resultSet.getString("password");
 
-                return user;
+                if(checkPass(user.getPassword(), hashedPass)){
+                    System.out.println("User Found...Validating");
+                    user.setName(resultSet.getString("name"));
+                    user.setSurname(resultSet.getString("surname"));
+                    user.setAge(resultSet.getInt("age"));
+                    user.setCountry(resultSet.getString("country"));
+                    user.setCity(resultSet.getString("city"));
+                    user.setGender(resultSet.getString("gender"));
+
+                    return user;
+
+                } else {
+                    System.out.println("Failed to Validate");
+                }
             }
+
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -153,7 +204,7 @@ public class DBUtils {
     }
 
 
-    public void enrollTo(EnrolledCourses enrolledCourses){
+    public void enrollTo(EnrolledCourses enrolledCourses) {
         loadDriver(dbDriver);
         Connection connection = getConnection();
         boolean status = false;
@@ -173,7 +224,6 @@ public class DBUtils {
     }
 
 
-
     public EnrolledCourses retrieveActiveCourses(User user) {
         loadDriver(dbDriver);
         Connection connection = getConnection();
@@ -188,7 +238,7 @@ public class DBUtils {
 
             status = resultSet.next();
 
-            if(status){
+            if (status) {
                 assert false;
                 enrolledCourses.setEmail(user.getEmail());
                 enrolledCourses.setCourse_id(resultSet.getInt("course_id"));
@@ -201,8 +251,6 @@ public class DBUtils {
         }
         return null;
     }
-
-
 
 
 }
--- src/main/java/utils/User.java
+++ src/main/java/utils/User.java
@@ -25,6 +25,7 @@ public class User {
 
     }
 
+
     public String getEmail() {
         return email;
     }
